14d2452ddfbeb2ce43916169ade66adfe8269c1c
vpra711 <vpr.achutharaman1000@outlook.com>
2023-05-03 20:51:53 +0530

    api done

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,6 +9,7 @@ namespace fdc
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
